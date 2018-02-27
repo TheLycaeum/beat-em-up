821968450a4d8fdd89544d131108ad41ab346017
Noufal Ibrahim <noufal@nibrahim.net.in>
2018-02-27 20:16:52 +0530

    Adds walking

--- game.py
+++ game.py
@@ -8,94 +8,139 @@ from pygame.locals import Rect, DOUBLEBUF, QUIT, K_ESCAPE, KEYDOWN, K_DOWN, \
 X_MAX = 640
 Y_MAX = 480
 
+def load_sprite(sheet, surf_rect, sprite_rect):
+    x0, y0, x1, y1 = sprite_rect
+    img = pygame.Surface(surf_rect, pygame.SRCALPHA, 32).convert_alpha()
+    img.blit(sheet, dest = (0, 0), area = sprite_rect)
+    return img
+
 class Fighter(pygame.sprite.Sprite):
-    def __init__(self, image, groups, pos, size):
+    IDLING = 0
+    PUNCHING = 1
+    WALKING = 2
+    
+    def __init__(self, image, groups, pos, background):
         super(Fighter, self).__init__()
-        self.s_x, self.s_y = size
         self.sheet = pygame.image.load(image).convert()
         colorkey = self.sheet.get_at((0,0))
         self.sheet.set_colorkey(colorkey)
-        self.image = pygame.Surface((self.s_x, self.s_y),pygame.SRCALPHA, 32).convert_alpha()
-        self.image.blit(self.sheet, dest = (0,0), area = (0,0,self.s_x, self.s_y))
-        
+        self.image = pygame.Surface((10, 10),pygame.SRCALPHA, 32).convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.midbottom = pos
-
+        self.rect.bottomleft = pos
         self.load_images()
-
-        self.punching = False
-
+        self.state = Fighter.IDLING
         self.add(groups)
+        self.background = background
+
+        self.fighter_pos = pos
+
 
     def load_images(self):
         # Load idling images
-        self.idle = []
+        self.idle_images = []
         self.idle_idx = 0
-        for i in range(0, self.s_x*7, self.s_x):
-            img = pygame.Surface((self.s_x, self.s_y), pygame.SRCALPHA, 32).convert_alpha()
-            img.blit(self.sheet, dest = (0, 0), area = (i, 0, i+self.s_x, self.s_y))
-            self.idle.append(img)
-        t = list(reversed(self.idle))
-        self.idle += t
-        self.idle.pop()
+        img = load_sprite(self.sheet, (166, 210), (8, 14, 8+163, 22+210))
+        self.idle_images.append(img)
+        img = load_sprite(self.sheet, (166, 210), (197, 14, 197+166, 26+210))
+        self.idle_images.append(img)
+        img = load_sprite(self.sheet, (166, 210), (382, 14, 382+164, 24+210))
+        self.idle_images.append(img)
+        img = load_sprite(self.sheet, (166, 210), (564, 14, 564+162, 18+210))
+        self.idle_images.append(img)
+        img = load_sprite(self.sheet, (166, 210), (750, 14, 750+162, 16+210))
+        self.idle_images.append(img)
+        img = load_sprite(self.sheet, (166, 210), (936, 14, 936+162, 16+210))
+        self.idle_images.append(img)
+        img = load_sprite(self.sheet, (166, 210), (1118, 14, 1118+162, 14+210))
+        self.idle_images.append(img)
+        self.idle_images += list(reversed(self.idle_images))
 
 
+        # Load punching images
         self.punch_images = []
         self.punch_idx = 0
-        # q = 1359
-        # s = 424
-        q = 1340
-        s = 424
-        w = 220
 
-        img = pygame.Surface((w, self.s_y), pygame.SRCALPHA, 32).convert_alpha()
-        img.blit(self.sheet, dest = (0, 0), area = (q, s, q+w, s+self.s_y))
+        img = load_sprite(self.sheet, (162, 210), (1380, 432, 1380+162, 432+210))
+        self.punch_images.append(img)
+        img = load_sprite(self.sheet, (222, 210), (1558, 434, 1558+222, 434+210))
+        self.punch_images.append(img)
+        img = load_sprite(self.sheet, (188, 210), (1796, 436, 1796+188, 436+210))
+        self.punch_images.append(img)
+        img = load_sprite(self.sheet, (162, 210), (1380, 432, 1380+162, 432+210))
         self.punch_images.append(img)
 
-        img = pygame.Surface((w, self.s_y), pygame.SRCALPHA, 32).convert_alpha()
-        img.blit(self.sheet, dest = (0, 0), area = (q+w, s, q+w*2, s+self.s_y))
-        self.punch_images.append(img)
 
-        img = pygame.Surface((w, self.s_y), pygame.SRCALPHA, 32).convert_alpha()
-        img.blit(self.sheet, dest = (0, 0), area = (q+w*2, s, q+w*3, s+self.s_y))
-        self.punch_images.append(img)
+        # Load walking images
+        self.walking_images = []
+        self.walking_idx = 0
+        
+        img = load_sprite(self.sheet, (166, 213), (2, 701, 2+166, 701+213))
+        self.walking_images.append(img)
+        img = load_sprite(self.sheet, (168, 212), (190, 700, 190+168, 700+212))
+        self.walking_images.append(img)
+        img = load_sprite(self.sheet, (168, 212), (380, 694, 380+168, 694+212))
+        self.walking_images.append(img)
+        img = load_sprite(self.sheet, (108, 220), (562, 692, 562+108, 692+220))
+        self.walking_images.append(img)
+        img = load_sprite(self.sheet, (106, 220), (698, 692, 698+106, 692+220))
+        self.walking_images.append(img)
+        img = load_sprite(self.sheet, (106, 222), (832, 690, 832+106, 690+222))
+        self.walking_images.append(img)
+        img = load_sprite(self.sheet, (150, 218), (968, 694, 968+150, 694+218))
+        self.walking_images.append(img)
 
-        # img = pygame.Surface((w, self.s_y), pygame.SRCALPHA, 32).convert_alpha()
-        # img.blit(self.sheet, dest = (0, 0), area = (q+w*2, s+self.s_y, q+w*3, s+self.s_y*2))
-        # self.punch_images.append(img)
 
-        # img = pygame.Surface((w, self.s_y), pygame.SRCALPHA, 32).convert_alpha()
-        # img.blit(self.sheet, dest = (0, 0), area = (q+w, s+self.s_y, q+w*2, s+self.s_y*2))
-        # self.punch_images.append(img)
-
-        # img = pygame.Surface((w, self.s_y), pygame.SRCALPHA, 32).convert_alpha()
-        # img.blit(self.sheet, dest = (0, 0), area = (q, s+self.s_y, q+w, s+self.s_y*2))
-        # self.punch_images.append(img)
-
-
+    def idle(self):
+        self.state = Fighter.IDLING
 
     def punch(self):
-        self.punching = True
+        self.state = Fighter.PUNCHING
+
+    def walk(self):
+        self.state = Fighter.WALKING
 
     def update(self):
-        if self.punching:
-            try:
-                self.image = self.punch_images[int(self.punch_idx)]
-                self.punch_idx += 0.25
-                self.punch_idx %= len(self.punch_images)
-                if self.punch_idx == 0:
-                    self.punching = False
-            except IndexError as e:
-                print (int(self.punch_idx))
-                raise
-        else:
-            self.image = self.image
-            self.image = self.idle[int(self.idle_idx)]
-            self.idle_idx += 0.25
-            self.idle_idx %= len(self.idle)
+        actions = {Fighter.PUNCHING : self.update_punch,
+                   Fighter.IDLING : self.update_idling,
+                   Fighter.WALKING: self.update_walking}
+        action = actions[self.state] 
+        action()
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = self.fighter_pos
 
-        x, y = self.rect.center
-        self.rect.center = (x, y)
+    def update_walking(self):
+        self.image = self.walking_images[int(self.walking_idx)]
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = self.fighter_pos
+        self.walking_idx += 0.5
+        self.walking_idx %= len(self.walking_images)
+        x, y = self.fighter_pos
+        x += 10
+        self.fighter_pos = x,y
+        if x == X_MAX*3.0/4:
+            self.background.scroll_right()
+            # self.fighter_pos = 100, y
+            
+        
+
+    def update_punch(self):
+        self.image = self.punch_images[int(self.punch_idx)]
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = self.fighter_pos
+        self.punch_idx += 0.5
+        self.punch_idx %= len(self.punch_images)
+        if self.punch_idx == 0:
+            self.state = Fighter.IDLING
+
+    def update_idling(self):
+
+        self.image = self.image
+        self.image = self.idle_images[int(self.idle_idx)]
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = self.fighter_pos
+        self.idle_idx += 0.5
+        self.idle_idx %= len(self.idle_images)
+
 
 
 class Background(pygame.sprite.Sprite):
@@ -155,7 +200,7 @@ def main():
 
     screen, empty = init_pygame(everything)
     b = Background("sprites/bg0.png", "sprites/bg1.png", everything)
-    f = Fighter("sprites/fighter-terry.png", everything, (200, 490), (185, 245))
+    f = Fighter("sprites/fighter-terry.png", everything, (100, 450), b)
 
     while True:
         for event in pygame.event.get():
@@ -163,17 +208,22 @@ def main():
                 sys.exit()
             if event.type == KEYDOWN:
                 if event.key == K_RIGHT:
-                    b.scroll_right()
+                    f.walk()
                     pass
                 if event.key == K_LEFT:
                     b.scroll_left()
                     pass
                 if event.key == K_a:
                     f.punch()
-                
+                if event.key == K_UP:
+                    f.jump()
+            if event.type == KEYUP:
+                if event.key == K_RIGHT:
+                    f.idle()
+                    
 
 
-        # clock.tick(30)
+        clock.tick(20)
 
         everything.clear(screen, empty)
         everything.update()
